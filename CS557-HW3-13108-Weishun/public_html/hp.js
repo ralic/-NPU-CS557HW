bba99f268a4c1177c60a45b9a99dd67ccafd1f2a
raliclo@gmail.com <raliclo@gmail.com>
2016-02-21 17:55:08 -0800

    update

---
+++ CS557-HW3-13108-Weishun/public_html/hp.js
@@ -0,0 +1,12 @@
+/*
+ * Student Info: Name=Lo,WeiShun , ID= 13108
+ * Subject: CourseNo_HWNo_Summer_2015
+ * Author: raliclo
+ * Filename: hp.js.js
+ * Date and Time: Feb 21, 2016 3:56:50 PM
+ * Project Name: CS557-HW3-13108-Weishun
+ */
+
+window.onload = function () {
+
+};
